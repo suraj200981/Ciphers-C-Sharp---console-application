851c9ae6f1c10ff803a98cbeb216ea70204c025f
ss2261 <ss2261@kent.ac.uk>
2021-09-08 20:10:34 +0100

    Add project files.

---
+++ CipherConsole/Caesar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CipherConsole
+{
+    class Caesar
+    {
+        public string inputStr { get; set; }
+        public int shiftKey { get; set; }
+
+         public Caesar(string str, int key )
+        {
+            inputStr = str;
+            shiftKey = key;
+        }
+
+
+        //Encode method
+        public void Encode() {
+
+
+            Console.Write(inputStr + "poo"+shiftKey);
+            Console.WriteLine("");
+            char temp;
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < inputStr.Length; i++) {
+
+                temp = (char)(inputStr[i] + shiftKey);
+                sb.Append(temp);
+            }
+            Console.WriteLine(sb);
+
+        }
+
+
+    }
+}
---
+++ CipherConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CipherConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "hello how are you";
+            var cipher1 = new Caesar(input, 3);
+
+
+            cipher1.Encode();
+            Console.ReadLine(); Moonpig364
+        }
+    }
+}
